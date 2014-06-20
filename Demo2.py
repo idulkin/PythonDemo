779871bad40a9264221e14318cda452b497cec7e
navlab <willy.iraheta@mindbodyonline.com>
2014-06-19 17:00:11 -0700

    Here is the commit from the second homework.

--- Demo2.py
+++ Demo2.py
@@ -1,32 +1,61 @@
 import webapp2
+import re
 
 form="""
-<form method="post" action='/rot13'>
-    <textarea cols="50" rows="25" name="text">
-    </textarea>
+<form method="post">
+    <b><font size="20">Signup</font></b>
     <br>
+    
+    <label>
+	&nbsp;        Username
+	<input type="text" name="username" value="%(username)s">
+	<div style="color: red">%(unError)s</div>
+    </label>
+    <br>
+    <label>
+	 &nbsp;       Password
+	<input type="password" name="password" value="%(password)s">
+	<div style="color: red">%(pwError)s</div>
+    </label>
+    <br>
+    <label>
+	&nbsp; Verify Password
+	<input type="password" name="vpassword" value="%(vpassword)s">
+	<div style="color: red">%(valError)s</div>
+    </label>
+    <br>
+    <label>
+	 &nbsp; Email(optional)
+	<input type="text" name="email" value="%(email)s">
+	<div style="color: red">%(emError)s</div>
+    </label>
+    <br>
+     
+   
     <input type="submit">
     <input type="reset">
 </form>	
 """
 
 class MainPage(webapp2.RequestHandler):
+    def writeForm(self,username="",password="",vpassword="",email="",unError="",pwError="",valError="",emError=""):
+        self.response.out.write(form %{"username": username,
+				       "password": password,
+				       "vpassword": vpassword,
+				       "email": email,
+				       "unError": unError,
+				       "pwError": pwError,
+				       "valError": valError,
+				       "emError": emError})
     def get(self):
-        self.response.out.write(form)
-
-class Cypher(webapp2.RequestHandler):
-    def writeForm(self,text):
-	self.response.out.write(form %{"text": text})
-
+	self.writeForm()
+    
     def post(self):
-	out = self.request.get('text')
-        self.writeForm(text=out)
-	self.response.out.write(out)
+        self.writeForm("That's a terrible username")
 
 
 app = webapp2.WSGIApplication([
     ('/', MainPage),
-    ('/rot13',Cypher),
 ], debug=True)
 
 
